60f55fbcdd00c9d121ee6bc59241544870c69f28
ar.sis <70283843+ardotsis@users.noreply.github.com>
2025-10-20 04:20:25 +0900

    update

--- scripts/test_vultr.bat
+++ scripts/test_vultr.bat
@@ -1,3 +1,3 @@
 @echo off
-docker build -f .\tests\Dockerfile.vultr -t dotfiles-vultr:latest . --no-cache
+docker build -f .\tests\Dockerfile.vultr -t dotfiles-vultr:latest .
 docker run --rm dotfiles-vultr:latest
--- src/install.sh
+++ src/install.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 REPO_URL="https://github.com/ardotsis/dotfiles.git"
 
-if [$1 = "vultr"]; then
-    echo "Vultr installation"
+if ["$1" = "vultr"]; then
+	echo "Vultr installation"
 else
-    echo "Something else"
+	echo "Something else"
 fi
