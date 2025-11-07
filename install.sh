cbd1d37aaa9d694b6abc3fe74db0a33bbaa021ef
ar.sis <70283843+ardotsis@users.noreply.github.com>
2025-11-07 23:34:54 +0900

    wip

--- install.sh
+++ install.sh
@@ -4,14 +4,12 @@ set -e -u -o pipefail -C
 ##################################################
 #                 Configurations                 #
 ##################################################
-# Paths
 readonly USERNAME="ardotsis"
 readonly DOTFILES_DIR="/home/$USERNAME/.dotfiles"
 readonly DOTFILES_SRC_DIR="$DOTFILES_DIR/dotfiles"
-readonly COMMON_HOME_DIR="$DOTFILES_SRC_DIR/common"
-readonly DEBUG_DOTFILES_DATA="/dotfiles"
 readonly DOTFILES_REPO="https://github.com/ardotsis/dotfiles.git"
-readonly INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/ardotsis/dotfiles/refs/heads/main/install.sh"
+readonly DOTFILES_LOCAL_REPO="/dotfiles"
+readonly DOTFILES_INSTALLER_URL="https://raw.githubusercontent.com/ardotsis/dotfiles/refs/heads/main/install.sh"
 
 # Parse script parameters
 while (("$#")); do
@@ -47,12 +45,6 @@ if [[ -z "${DEBUG+x}" ]]; then
 	readonly DEBUG=false
 fi
 
-if [[ $(id -u) -eq 0 ]]; then
-	readonly SUDO=""
-else
-	readonly SUDO="sudo"
-fi
-
 # Validate host
 case "$HOST" in
 vultr)
@@ -66,6 +58,19 @@ arch)
 	exit 1
 	;;
 esac
+
+readonly COMMON_HOME_DIR="$DOTFILES_SRC_DIR/common"
+readonly HOST_HOME_DIR="$DOTFILES_SRC_DIR/$HOST"
+readonly HOST_SUFFIX=".$HOST"
+
+# Set sudo mode
+if [[ $(id -u) -eq 0 ]]; then
+	readonly SUDO=""
+else
+	if [[ "$OS" == "debian" ]]; then
+		readonly SUDO="sudo"
+	fi
+fi
 
 ##################################################
 #                Common Functions                #
@@ -87,9 +92,12 @@ print_header() {
 _log() {
 	local level="$1"
 	local msg="$2"
+	local timestamp
+
+	timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
 
 	if [[ "$DEBUG" == "true" ]]; then
-		printf "[%s] %s\n" "$level" "$msg"
+		printf "[%s] [%s] %s\n" "$timestamp" "$level" "$msg"
 	fi
 }
 
@@ -111,7 +119,7 @@ log_vars() {
 		fi
 	done
 
-	_log "DEBUG_VARS" "$msg"
+	_log "DEBUG_VAR" "$msg"
 }
 
 log_info() {
@@ -181,37 +189,11 @@ find_depth_1() {
 }
 
 fetch_config_path() {
-	# local common_dir="$DOTFILES_DIR/dotfiles/common"
-	# local host_dir="$DOTFILES_DIR/dotfiles/host/$HOST"
-	local item
-	local basename
-
-	# scan common directory and save paths
-	# include host's exceptions
-
-	# has_host() {
-	# 	local $item="$1"
 	# 	local basename=${item##*/}
-
-	# 	if [[ -f "$host_dir/$basename"  ]]
-	# }
+	# if [[ $item == *."$HOST" ]]; then
 
 	while read0 "item"; do
-		basename=${item##*/}
-		log_vars "basename"
-
-		if [[ -f $item ]]; then
-
-			echo "file"
-		elif [[ -d $item ]]; then
-			echo "dir"
-		fi
-
-		# if [[ $item == *."$HOST" ]]; then
-		# 	echo "host item: $item"
-		# else
-		# 	echo "common item: $item"
-		# fi
+		log_vars "item"
 	done < <(find_depth_1 "$COMMON_HOME_DIR")
 }
 
@@ -233,7 +215,7 @@ clone_dotfiles_repo() {
 	if [[ $from == "git" ]]; then
 		git clone -b main "$DOTFILES_REPO" $DOTFILES_DIR
 	elif [[ $from == "local" ]]; then
-		cp -r "$DEBUG_DOTFILES_DATA" "$DOTFILES_DIR"
+		cp -r "$DOTFILES_LOCAL_REPO" "$DOTFILES_DIR"
 		chown -R "$USERNAME:$USERNAME" "$DOTFILES_DIR"
 	fi
 }
@@ -259,8 +241,6 @@ do_setup_vultr() {
 	else
 		clone_dotfiles_repo "git"
 	fi
-	# print_header "Install Neovim"
-	# install_package "neovim"
 
 	fetch_config_path
 }
@@ -270,37 +250,20 @@ do_setup_arch() {
 }
 
 main() {
-	log_debug "Start main func"
-	log_vars "SUDO"
-
-	# Download install.sh and run locally
-	# if [[ -f "$0" ]]; then
-	# 	script_path="/var/tmp/install.sh"
-	# 	log_info "Downloading install script..."
-	# 	curl -fsSL "$INSTALL_SCRIPT_URL" -o $script_path
-	# 	chmod +x $script_path
-	# 	$script_path "$@"
-	# 	exit 0
-	# fi
-
-	if [[ -z $HOST ]]; then
-		log_error "Please specify the host name using '--host (-h)' parameter."
-		exit 1
-	fi
-
-	log_vars "HOST" "IS_SETUP" "DEBUG"
+	log_info "Start installation..."
+	log_vars "HOST" "IS_SETUP" "DEBUG" "SUDO" "OS"
 
 	if [[ "$IS_SETUP" == "true" ]]; then
 		"do_setup_${HOST}"
 	else
 		local passwd
+
 		passwd=$(get_random_str 32)
 		add_ardotsis_chan "$passwd"
 		log_info "Password for ar.sis: $passwd"
 
 		script_path=$(get_script_path)
 		log_vars "script_path"
-		# TODO: download the script
 		local cmd=("$script_path" "--host" "$HOST" "--setup")
 		if [[ "$DEBUG" == "true" ]]; then
 			cmd+=("--debug")
